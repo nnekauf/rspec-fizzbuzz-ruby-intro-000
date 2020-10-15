c8fe35dd25b1627a803f482f07ab8751f6f8b0ed
industrious-chip-8083 <ufumakanneka@gmail.com>
2020-10-15 15:00:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
- if
-   puts
-  elsif
-    puts
+ if number % 3== 0
+   puts "fizz"
+  elsif number % 5 == 0
+    puts "buzz"
   else
     puts "nil"
    
