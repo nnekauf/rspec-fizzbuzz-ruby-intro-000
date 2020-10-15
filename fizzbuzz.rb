3b25a27faf90a8e14dfc6645cfec17c17aead39c
industrious-chip-8083 <ufumakanneka@gmail.com>
2020-10-15 14:58:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
- 
+def fizzbuzz(number)
+ if
+   puts
+  elsif
+    puts
+  else
+    puts "nil"
+   
 end
